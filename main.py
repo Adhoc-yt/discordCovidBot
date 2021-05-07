e0b360326e87190269e820d3189c3f9b115a1a52
Adhoc <adhoc@fennecdev.com>
2021-05-07 17:27:12 -0500

    %pcr pour tester un utilisateur

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 from datetime import datetime
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="//")
+bot = commands.Bot(command_prefix="%")
 
 
 @bot.event
@@ -15,11 +15,30 @@ async def on_ready() -> None:
 async def get_covid(message):
     role = discord.utils.get(message.author.guild.roles, name="Covided")
     await message.author.add_roles(role)
-    await message.channel.send(f"Hey @Covided, {message.author.name} est maintenant {role.name}")
+    await message.channel.send(f"{message.author.name} est maintenant {role.name}")
+
+
+@bot.command(pass_context=True)
+async def pcr(ctx, user: discord.Member):
+    """
+    PCR test - Usage: %pcr @member
+    :return: if member has covid or not
+    """
+    role = discord.utils.find(lambda r: r.name == 'Covided', ctx.message.guild.roles)
+    if role in user.roles:
+        await ctx.send("{} a le Covid".format(user))
+    else:
+        await ctx.send("{} est sain".format(user))
 
 
 @bot.event
 async def on_message(message):
+    # We need this line to allow usage of bot.commands()
+    await bot.process_commands(message)
+
+    # Logging
+    # print("{}".format(message.content.lower()))
+
     if message.author == bot.user:
         return
 
@@ -36,7 +55,6 @@ async def on_message(message):
 
     if ''.join(filter(str.isalpha, message.content.lower())).endswith("quoi") \
             and not message.content.lower().endswith(">"):
-        print("{} - ".format(message.content.lower()))
         await message.channel.send("FEUR!")
 
 
