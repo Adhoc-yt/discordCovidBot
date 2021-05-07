a4cbf9a3128593ab399c419acd5c65311469ee63
Adhoc <adhoc@fennecdev.com>
2021-05-07 15:47:42 -0500

    fonctions bonus + get covided en disant pangolin

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import discord
 from datetime import datetime
 from discord.ext import commands
@@ -10,18 +11,32 @@ async def on_ready() -> None:
     print("{} : Démarrage de bot".format(datetime.now()))
 
 
+@bot.command()
+async def get_covid(message):
+    role = discord.utils.get(message.author.guild.roles, name="Covided")
+    await message.author.add_roles(role)
+    await message.channel.send(f"Hey @Covided, {message.author.name} est maintenant {role.name}")
+
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('ping'):
-        await message.channel.send('Pong!')
+    if "pangolin" in message.content:
+        await get_covid(message)
 
+    # FONCTIONS BONUS
     if "covid" in message.content:
         await message.channel.send("On m'a appelé?")
 
-    if message.content.endswith("quoi"):
+    # détection cyrillique => cyka
+    if re.search('[а-яА-Я]', message.content):
+        await message.channel.send("сука блять")
+
+    if ''.join(filter(str.isalpha, message.content.lower())).endswith("quoi") \
+            and not message.content.lower().endswith(">"):
+        print("{} - ".format(message.content.lower()))
         await message.channel.send("FEUR!")
 
 
