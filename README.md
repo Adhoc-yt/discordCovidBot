087761731d763cecc81844ee22b48f98a90e5ad3
Adhoc <adhoc@fennecdev.com>
2021-05-10 18:50:20 -0500

    Dr Raoult peut utiliser %chloroquine pour soigner (ou peut-être tuer) quelqu'un, a un cooldown

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Exploring Discord bot API on https://twitch.tv/adhoc_yt
 
 #Setup
-https://discord.com/api/oauth2/authorize?client_id=839618964304560159&permissions=268504064&scope=bot
+https://discord.com/api/oauth2/authorize?client_id=839618964304560159&permissions=402738178&scope=bot
--- main.py
+++ main.py
@@ -9,10 +9,16 @@ intents.members = True
 bot = commands.Bot(command_prefix="%", intents=intents)
 transmission_rate = .5
 
+# TODO: ajouter les rôles Covided et Dr Raoult sur des nouveaux serveur
+# Fonction infection_passive, qui infecte les membres côte à côte dans la liste alphabétique des users
+# Fonction %geste_barrière, divise par 3 la proba de choper le covid
+# Fonction %stats, qui indique le nombre de personnes infectées sur l'ensemble du serveur, hors bot
+# Si Covid, nickname suffixe peu glorieux (le raciste, le gros, le pédants, etc), une fois guéri on l'enlève
+
 
 def risk_infection(message):
-    role = discord.utils.find(lambda r: r.name == 'Covided', message.guild.roles)
-    return role in message.guild.get_member(message.author.id).roles
+    role_covid = discord.utils.find(lambda r: r.name == 'Covided', message.guild.roles)
+    return role_covid in message.guild.get_member(message.author.id).roles
 
 
 @bot.event
@@ -20,18 +26,62 @@ async def on_ready() -> None:
     print("{} : Démarrage de bot".format(datetime.now()))
 
 
-@bot.command()
 async def get_covid(message):
     role = discord.utils.get(message.author.guild.roles, name="Covided")
     await message.author.add_roles(role)
     await message.channel.send(f"{message.author.name} est maintenant {role.name}")
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        em = discord.Embed(title=f"Rupture de stock",
+                           description=f"Oh non Docteur, nous n'avons plus de chloroquine!\n\
+                                        Nous en aurons à nouveau dans {error.retry_after:.2f} secondes.")
+        await ctx.send(embed=em)
+
+
+# Si le membre a le covid, seul Dr Raoult peut utiliser la commande Chloroquine sur ce membre
+# Cependant, la cholorquine a X% de chances de tuer le patient (kick serveur)
+# Comme la chloroquine devient rare, Dr Raoult ne peut utiliser la choloroquine que 2 fois toutes les minutes
+@bot.command(pass_context=True)
+@commands.has_role('Dr Raoult le Fédérateur')
+@commands.cooldown(1, 120, commands.BucketType.guild)
+async def chloroquine(ctx, user_patient: discord.Member):
+    """
+    Dose de choloroquine - Usage: %chloroquine @member
+    :return: null, a une chance de soigner du Covid ou de kick le membre
+    """
+    role_covid = discord.utils.find(lambda r: r.name == 'Covided', ctx.message.guild.roles)
+    proba_kick = 0.05
+    # Idée historique patient, injections ratées augmentent risque de mort
+    proba_guerison = 0.7
+
+    if role_covid in user_patient.roles:
+        if random.random() <= proba_kick:
+            await user_patient.send("Le Covid n'a pas eu raison de toi, mais le docteur, oui.\n\
+Pour revenir: https://discord.gg/6QEvgHWnM3")
+            await ctx.guild.kick(user_patient, reason="Chloroquined")
+            await ctx.send(f"{user_patient} n'a pas survécu à sa dose de choloroquine!")
+        elif random.random() <= proba_guerison:
+            await user_patient.remove_roles(role_covid)
+            await ctx.send(f"{user_patient.name} est guéri, merci Docteur!")
+        else:
+            await ctx.send("Le patient n'est pas guéri... Mais au moins, il n'est pas mort !")
+    else:
+        replies = [
+            "Mais enfin Docteur, ce patient est sain",
+            "Docteur, vous avez (encore) bu?",
+            "Ce patient est guéri !... Mais n'était pas malade."
+        ]
+        await ctx.send(random.choice(replies))
+
+
 @bot.command(pass_context=True)
 async def pcr(ctx, user: discord.Member):
     """
     PCR test - Usage: %pcr @member
-    :return: if member has covid or not
+    :return: si le membre a le covid ou pas
     """
     role = discord.utils.find(lambda r: r.name == 'Covided', ctx.message.guild.roles)
     if role in user.roles:
@@ -42,14 +92,22 @@ async def pcr(ctx, user: discord.Member):
 
 @bot.event
 async def on_message(message):
-    # We need this line to allow usage of bot.commands()
+    # Besoin de cette ligne pour ne pas bloquer les bot.commands()
     await bot.process_commands(message)
+    # Logging
+    last_message = await message.channel.history(limit=2).flatten()
+    last_message = last_message[1]
+    print("{} répond à {} - {}".format(message.author, last_message.author, message.content.lower()))
     if message.author == bot.user:
         return
 
     # FONCTIONS BONUS
     if "covid" in message.content:
         await message.channel.send("On m'a appelé?")
+    if "pied" in message.content:
+        await message.channel.send("Ah, ça parle de pied? <@!830199237856723004> est fétichiste!")
+    if "éthique" in message.content:
+        await message.channel.send("> L'éthique, c'est pour les pauvres.\n- <@!307964302533591050>")
 
     # détection cyrillique => cyka
     if re.search('[а-яА-Я]', message.content):
@@ -59,13 +117,8 @@ async def on_message(message):
             and not message.content.lower().endswith(">"):
         await message.channel.send("FEUR!")
 
-    # Logging
-    last_message = await message.channel.history(limit=2).flatten()
-    last_message = last_message[1]
-    print("{} répond à {} - {}".format(message.author, last_message.author, message.content.lower()))
-
-    if risk_infection(last_message):
-        if risk_infection(message) or random.random() > transmission_rate:
+    if not message.author.bot and risk_infection(last_message):
+        if risk_infection(message) or random.random() >= transmission_rate:
             return
         else:
             await get_covid(message)
